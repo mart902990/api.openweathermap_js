da340c0e87ce0b1295fe7624dfe55bb93557f690
mart902990 <mart902990@gmail.com>
2021-12-24 23:35:07 +0700

    add

---
+++ 1.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){  //событие ready фуния выполнится гда DOM загрузится полностью
+ //$.
+ $.get(
+    "http://api.openweathermap.org/data/2.5/weather",
+    {  //"id": "569143 " , 
+     " q": "London",
+      "apiid": "27a1d09d4954cd2b045e899fe11c76fb",
+   }, 
+    function(data) {console.log(data); }
+ ) ;  
+}
+);
